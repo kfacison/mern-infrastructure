8cbb5af4fc18e1a922419e77f78e20ebc2bcb8a7
kfac <kfacison1998@gmail.com>
2023-11-10 17:25:40 -0600

    add sign up form and uttilites folder

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,9 +2,9 @@ import SignUpForm from "../../components/SignUpForm/SignUpForm";
 
 export default function AuthPage(){
     return (
-        <>
+        <main>
         <h1>AuthPage</h1>
         <SignUpForm />
-        </>
+        </main>
     );
 }
