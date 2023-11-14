979037fe398c92d6efcbf0aab9470ef4f25b9d74
kfac <kfacison1998@gmail.com>
2023-11-14 12:14:37 -0600

    login connected to backend

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -20,5 +20,18 @@ export async function signUp(userData) {
 }
 
 export async function login(credentials){
-
+    const res = await fetch(BASE_URL+"/login", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        // Fetch requires data payloads to be stringified
+        // and assigned to a body property on the options object
+        body: JSON.stringify(credentials)
+    });
+    // Check if request was successful
+    if (res.ok) {
+        // res.json() will resolve to the JWT
+        return res.json();
+    } else {
+        throw new Error('Invalid Login');
+    }
 }
